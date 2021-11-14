a9d759ccb2da0dec757fdc8cd306516b9748703b
maximkurochka <kurochka.maxim@gmail.com>
2021-11-14 21:54:18 +0200

    Implement Inject() and Eject() function

--- Injector/Injector.cpp
+++ Injector/Injector.cpp
@@ -1,20 +1,151 @@
 ﻿// Injector.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
 //
+// Разработать DLL Injector использующий CreateRemoteThread. протестировать на "Hello World" динамической библиотеке
 
 #include <iostream>
+#include <Windows.h>
+#include <tlhelp32.h>
+
+bool InjectDllToTheProcess(DWORD processId, PCWSTR dllPath)
+{
+	bool res = false;
+	HANDLE targetProcess = NULL;
+	HANDLE remoteThread = NULL;
+	LPWSTR remoteBuff = NULL;
+
+	__try
+	{
+		targetProcess = OpenProcess(PROCESS_CREATE_THREAD | PROCESS_VM_OPERATION | PROCESS_VM_WRITE, FALSE, processId);
+		if (targetProcess == NULL)
+		{
+			__leave;
+		}
+
+		int buffSize = (lstrlenW(dllPath) + 1) * sizeof(WCHAR);
+		remoteBuff = reinterpret_cast<LPWSTR>(VirtualAllocEx(targetProcess, NULL, buffSize, MEM_COMMIT, PAGE_READWRITE));
+		if (remoteBuff == NULL)
+		{
+			__leave;
+		}
+
+		if (!WriteProcessMemory(targetProcess, remoteBuff, dllPath, buffSize, NULL))
+		{
+			__leave;
+		}
+
+		PTHREAD_START_ROUTINE loadLibraryFunction = reinterpret_cast<PTHREAD_START_ROUTINE>(GetProcAddress(GetModuleHandleW(L"Kernal32"), "LoadLibraryW"));
+		if (loadLibraryFunction == NULL)
+		{
+			__leave;
+		}
+
+		remoteThread = CreateRemoteThread(targetProcess, NULL, 0, loadLibraryFunction, remoteBuff, 0, NULL);
+		if (remoteThread == NULL)
+		{
+			__leave;
+		}
+
+		WaitForSingleObject(remoteThread, INFINITE);
+
+		res = true;
+	}
+	__finally
+	{
+		if (remoteBuff != NULL)
+		{
+			VirtualFreeEx(targetProcess, remoteBuff, 0, MEM_RELEASE);
+		}
+
+		if (remoteThread != NULL)
+		{
+			CloseHandle(remoteThread);
+		}
+
+		if (targetProcess != NULL)
+		{
+			CloseHandle(targetProcess);
+		}
+	}
+
+	return res;
+}
+
+bool EjectDllFromTheProcess(DWORD processId, PCWSTR dllPath) 
+{
+	bool res = false;
+	HANDLE threadsSnapshot = NULL;
+	HANDLE targetProcess = NULL;
+	HANDLE remoteThread = NULL;
+
+	__try 
+	{
+		threadsSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPMODULE, processId);
+		if (threadsSnapshot == NULL)
+		{
+			__leave;
+		}
+
+		MODULEENTRY32W me;
+		me.dwSize = sizeof(me);
+		BOOL fFound = FALSE;
+		BOOL fMoreMods = Module32FirstW(threadsSnapshot, &me);
+		for (; fMoreMods; fMoreMods = Module32NextW(threadsSnapshot, &me))
+		{
+			fFound = (lstrcmpiW(me.szModule, dllPath) == 0) || (lstrcmpiW(me.szExePath, dllPath) == 0);
+			if (fFound)
+			{
+				break;
+			}
+		}
+		if (!fFound) 
+		{
+			__leave;
+		}
+
+		targetProcess = OpenProcess(PROCESS_CREATE_THREAD | PROCESS_VM_OPERATION, FALSE, processId);
+		if (targetProcess == NULL)
+		{
+			__leave;
+		}
+
+		PTHREAD_START_ROUTINE pfnThreadRtn = reinterpret_cast<PTHREAD_START_ROUTINE>(GetProcAddress(GetModuleHandle(L"Kernel32"), "FreeLibrary"));
+		if (pfnThreadRtn == NULL)
+		{
+			__leave;
+		}
+
+		remoteThread = CreateRemoteThread(targetProcess, NULL, 0, pfnThreadRtn, me.modBaseAddr, 0, NULL);
+		if (remoteThread == NULL)
+		{
+			__leave;
+		}
+
+		WaitForSingleObject(remoteThread, INFINITE);
+
+		res = true;
+	}
+	__finally 
+	{ 
+		if (threadsSnapshot != NULL)
+		{
+			CloseHandle(threadsSnapshot);
+		}
+
+		if (remoteThread != NULL)
+		{
+			CloseHandle(remoteThread);
+		}
+
+		if (targetProcess != NULL)
+		{
+			CloseHandle(targetProcess);
+		}
+	}
+
+	return res;
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	return 0;
 }
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
